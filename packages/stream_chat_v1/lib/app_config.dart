d07d6e70c17742f7337c2397dbde2210a1f077af
Fabrizio Moscon <mosconfabrizio@gmail.com>
2021-11-29 10:56:10 +0000

    feat: edit config for dev-local

--- packages/stream_chat_v1/lib/app_config.dart
+++ packages/stream_chat_v1/lib/app_config.dart
@@ -1,13 +1,13 @@
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-const kDefaultStreamApiKey = 'em6azxphbfs9';
+const kDefaultStreamApiKey = 'm7dwyc5hqjwf';
 
 final defaultUsers = <String, User>{
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiQ2FwdGFpbi1FZGlzb24ifQ._Q3QX-_oSZHzu7agAEV2eW4z_E1UvHO5euMptgbkUb0':
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzgxODI2MTgsInVzZXJfaWQiOiJjOTVjYzYzNi1kNWMzLTRjNzYtODY4OS1mZjQ1ZGI2MTE3Y2MifQ.SSI6ln8ad202K3jy03lg3H1f9dwngXnCirrCgnw26Mo':
       User(
-    id: 'Captain-Edison',
+    id: 'c95cc636-d5c3-4c76-8689-ff45db6117cc',
     extraData: {
-      'name': 'Captain Edison',
+      'name': 'c95cc636-d5c3-4c76-8689-ff45db6117cc',
       'image':
           'https://avatars.githubusercontent.com/u/265409?s=400&u=2d0e3bb1820db992066196bff7b004f0eee8e28d&v=4',
     },
